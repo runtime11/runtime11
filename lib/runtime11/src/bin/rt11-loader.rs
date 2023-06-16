8e883609b0ddd5766ce62899425911a23cd11c99
David Rheinsberg <david@readahead.eu>
2023-06-16 11:01:41 +0200

    runtime11: work around ARM unwinding ABI
    Define the ARM unwinding personality routines to fix linking on ARM.
    These are usually defined by libgcc (or whatever unwinding library is
    used). For unwinding to work, this needs to be pulled in.
    
    Unfortunately, even with unwinding disable as we do, these symbols are
    still pulled in by libcore and surprisingly not stripped properly during
    linking. Hence, provide them as stubs for now and decide on a better
    home later.
    
    Signed-off-by: David Rheinsberg <david@readahead.eu>

--- lib/runtime11/src/bin/rt11-loader.rs
+++ lib/runtime11/src/bin/rt11-loader.rs
@@ -13,6 +13,66 @@ fn panic_handler(_info: &core::panic::PanicInfo) -> ! {
     loop {}
 }
 
+/// ARM EHABI Compact Personality Routines
+///
+/// The ARM EHABI defines a compact format for exception handler tables, which
+/// embed the index of the pre-defined personality routine to use. Those
+/// routines are called `__aeabi_unwind_cpp_pr?()` (with only `0`, `1`, and `2`
+/// currently being defined). It is up to the platform to implement these
+/// pre-defined routines. They have to follow the ARM-EHABI specification.
+///
+/// The `core` crate is always compiled with `panic=unwind`, but allows linking
+/// into `panic=abort` applications. This avoids shipping `core` twice with
+/// every compiler release. In most cases, final linking strips all unwind
+/// tables and references. However, for some reason this does not reliably work
+/// on ARM and the exception-tables remain. These contain dummy-relocations
+/// that reference the pre-defined personality routines. This either is a
+/// limitation of the platform, or a limitation of rustc. Regardless, we have
+/// to provide those to satisfy the linker, even though we never end up raising
+/// exceptions in our panic handler.
+///
+/// We simply return `_URW_FAILURE` (`9`), an ARM-specific unwinding reason
+/// denoting failure to run the personality routine. But since they should
+/// really never be called, this should not matter much.
+///
+/// XXX: We shouldn't be required to copy these everywhere. For now, we keep
+///      them here, but we should either fix rustc or find a better home.
+#[cfg(target_arch = "arm")]
+#[no_mangle]
+extern "C" fn __aeabi_unwind_cpp_pr0(
+    _state: u32,
+    _cb: *const core::ffi::c_void,
+    _ctx: *const core::ffi::c_void,
+) -> i32 {
+    return 9;
+}
+
+/// ARM EHABI Compact Personality Routines
+///
+/// See `__aeabi_unwind_cpp_pr0()` for details.
+#[cfg(target_arch = "arm")]
+#[no_mangle]
+extern "C" fn __aeabi_unwind_cpp_pr1(
+    _state: u32,
+    _cb: *const core::ffi::c_void,
+    _ctx: *const core::ffi::c_void,
+) -> i32 {
+    return 9;
+}
+
+/// ARM EHABI Compact Personality Routines
+///
+/// See `__aeabi_unwind_cpp_pr0()` for details.
+#[cfg(target_arch = "arm")]
+#[no_mangle]
+extern "C" fn __aeabi_unwind_cpp_pr2(
+    _state: u32,
+    _cb: *const core::ffi::c_void,
+    _ctx: *const core::ffi::c_void,
+) -> i32 {
+    return 9;
+}
+
 fn exit(code: usize) -> usize {
     let sc = rt11_ffi_linux::native::syscall::Syscall { };
 
